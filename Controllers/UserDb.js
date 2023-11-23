858bd7c4134586f7f47f25c427df5df0dc9038ca
Nikolai <nikolai2002b@gmail.com>
2023-11-23 13:53:37 +0100

    Post user api

--- Controllers/UserDb.js
+++ Controllers/UserDb.js
@@ -22,4 +22,24 @@ UserDb.post("/post", (req, res) => {
   );
 });
 
+UserDb.get("/users/:userid", (req, res) => {
+  const { userid } = req.params;
+
+  const SELECT_USER_QUERY = `SELECT * FROM users WHERE userid = ?`;
+
+  pool.query(SELECT_USER_QUERY, [userid], (err, results) => {
+    if (err) {
+      console.error("Error fetching user:", err);
+      return res.status(500).json({ error: "Error fetching user" });
+    }
+
+    if (results.length === 0) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const user = results[0]; // Assuming there's only one user for a given userid
+    return res.status(200).json({ user });
+  });
+});
+
 export default UserDb;
